e617ca37ed8457e1a1c703ceffc9836a53fd1a1d
Chris Hinds <chris@chrishinds.com>
2013-12-30 11:50:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cahal::Application.config.secret_key_base = '9c0d2d0c217276b4cddb7b4263c7d7f6dbc30ebe8bd265abe5c96ea16f85ba136a6a2e8c29bae755fac460f1d119aba45601faff78f53f034e61a516bc5163f5'
